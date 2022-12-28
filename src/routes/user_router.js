5181cec77c29c71942a5e776613fea6d6a66ba0c
Shreyansh Agrawal <ashreyansh47@gmail.com>
2022-12-28 14:36:59 +0530

    added login functionality

--- src/routes/user_router.js
+++ src/routes/user_router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const user = require('../models/User');
+const bcrypt = require('bcrypt')
 
 router.get('/allUsers', async (req, res) => {
     var users = await user.find();
@@ -11,7 +12,16 @@ router.post('/addUser', async (req, res) => {
     await user.deleteOne({ userid: req.body.userid })
     await user.deleteOne({ email: req.body.email })
     await user.deleteOne({ phone: req.body.phone })
-    const newUser = new user(req.body)
+    const userdata = req.body; 
+
+    //Encrypted the password
+    const password = userdata.password
+    const salt = await bcrypt.genSalt(7)
+    const encryptpassword = await bcrypt.hash(password, salt); 
+    userdata.password = encryptpassword; 
+
+    // adding to the database
+    const newUser = new user(userdata)
     await newUser.save(function (err) {
         if (err) {
             const response = { success: false, error: err };
@@ -22,6 +32,26 @@ router.post('/addUser', async (req, res) => {
         console.log('new User added')
         res.json(response);
     });
+})
+
+router.post('/login', async (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const founduser = await user.findOne({ email: email })
+    if (!founduser) {
+        res.json({ success: false, error: "user not found" }); 
+        return; 
+    }
+
+    const correctpassword = await bcrypt.compare(password, founduser.password); 
+    
+    if (!correctpassword) {
+        res.json({ success: false, error: "password not matched" })
+        return 
+    }
+    res.json({success: true,data:founduser})
+
 })
 
 
